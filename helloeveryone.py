48512407656f5091437e601fe1598916f51a1a13
Robin Glory <yannaingkyawtint@gmail.com>
2024-11-03 16:55:33 +0630

    Updating the helloeveryone.py

--- helloeveryone.py
+++ helloeveryone.py
@@ -1,2 +1,15 @@
-name = input("What is your name??")
-print("Hello, ", name , ", Nice to meet you!")
+#This is just testing comment.!
+#This program is used for asking  name!
+# name = input("What is your name??")
+# print("Hello,", name , ", Nice to meet you!", sep = '????', end='')
+# print("What tf is going on here!!")
+
+# name = input("What is your name?")
+# name = name.strip().title() #Remove whitespace from string ##Trying to capatalize the user name!
+
+name = input("What is your name?").strip().title()
+# name = name.strip().title() #Remove whitespace from string ##Trying to capatalize the user name!
+
+print(f"Hello, {name}")
+# print('Hello, i am using the "double quotes"');
+# print("Hello, this is using the blackslash \"Friend\"")
