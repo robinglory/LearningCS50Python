2ce39334757949c83a98d484b7eb80b34f1068c6
Robin Glory <koyannaingkyawtint@gmail.com>
2025-01-08 23:11:33 +0630

    Library

--- Lesson 4 Library/itunes.py
+++ Lesson 4 Library/itunes.py
@@ -7,6 +7,16 @@
 # response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
 # print(response.json())
 
+# import json
+# import requests
+# import sys
+
+# if len(sys.argv) != 2:
+#     sys.exit()
+
+# response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+# print(json.dumps(response.json(), indent=2))
+
 import json
 import requests
 import sys
@@ -14,5 +24,8 @@ import sys
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(json.dumps(response.json(), indent=2))
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])
